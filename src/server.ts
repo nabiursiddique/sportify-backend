058063361362eeb0a9a459999e36f068dde38e1d
Nabiur siddique <nabiursiddique01@gmail.com>
2024-07-11 16:12:57 +0600

    Basic server setup is done

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import config from "./config";
+
+async function main() {
+  try {
+    await mongoose.connect(config.db_url as string);
+
+    const port = config.port;
+    app.listen(port, () => {
+      console.log(`Sportify app listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
